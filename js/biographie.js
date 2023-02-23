96de86ab6c7b70291bf7ff431d644be217c28b17
haussmannlife <125928296+haussmannlife@users.noreply.github.com>
2023-02-23 23:26:41 +0100

    Add files via upload

---
+++ js/biographie.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("input.slider").on("input change", function(event) {
+        var element = $(this).parents("div.container");
+        var pos = event.target.value;
+        
+        element.find("div.before").css({width: pos + "%"});
+        element.find("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
+    });
+});
